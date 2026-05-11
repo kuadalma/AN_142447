42a6b21ad336d61e14e750427b843af31988b296
Kuadalma <adammarzec2808@gmail.com>
2026-05-11 21:26:22 +0200

    wczytywanie danych z pliku

--- main.py
+++ main.py
@@ -1,1 +1,26 @@
 #Projekt AN
+
+import numpy as np
+
+def wczytaj_dane(plik):
+    dane = {}
+
+    with open(nazwa_pliku, 'r') as plik:
+        for linia in plik:
+            elementy = linia.strip().split(',')
+
+            x = float(elementy[0])
+            y = float(elementy[1])
+            fxy = float(elementy[2])
+
+            if y not in dane:
+                dane[y] = {'x': [], 'fx': []}
+
+            dane[y]['x'].append(x)
+            dane[y]['fx'].append(fxy)
+
+    return dane
+
+
+plik = 'Dane/142447.txt'
+dane = wczytaj_dane(plik)
